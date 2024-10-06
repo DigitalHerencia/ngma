2c819a6be90a8a6b6434aaae1c0f2cc158daab71
DigitalHerencia <digitalherencia@outlook.com>
2024-10-05 22:20:41 -0600

    added assets and custom page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,111 @@
-import Image from "next/image";
+"use client"
+
+import { useSession, signIn, signOut } from "next-auth/react"
+import Image from "next/image"
+import {
+    EnvelopeIcon,
+    ShieldCheckIcon,
+    UserCircleIcon,
+} from "@heroicons/react/24/solid"
+import React from "react"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const { data: session } = useSession()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+        <div className="relative min-h-screen bg-black text-white">
+            {/* Background Shapes */}
+            <div className="fixed top-0 left-0 w-[225%] h-[225%] translate-x-[5%] -translate-y-[27%] transform rotate-180">
+                <Image
+                    className="object-contain opacity-30"
+                    src="/SHAPES_BLACK.png"
+                    alt="Background Shapes"
+                    fill
+                />
+            </div>
+
+            {/* Main content with logo and text */}
+            <div className="relative grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-bebas-neue)] z-10">
+                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                    {/* Logo */}
+                    <Image
+                        src="/NGMA-AUTH-BLACK.png"
+                        alt="NextGen Management Agency logo"
+                        width={256}
+                        height={75}
+                        priority
+                    />
+
+                    {/* Main content */}
+                    <h1 className="text-4xl sm:text-5xl font-bold text-center sm:text-left">
+                        Welcome to NextGen Management Agency
+                    </h1>
+                    <p className="text-lg sm:text-xl text-center sm:text-left">
+                        Manage talent and clients seamlessly with our all-in-one
+                        solution for agencies.
+                    </p>
+
+                    <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-bebas-neue)]">
+                        <li className="mb-2">
+                            {session ? (
+                                <>
+                                    Signed in as {session.user?.email}.{" "}
+                                    <button onClick={() => signOut()}>
+                                        Sign out
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <button onClick={() => signIn("auth0")}>
+                                        To get started, sign in to your account.
+                                    </button>
+                                </>
+                            )}
+                        </li>
+                        <li>
+                            Manage your talent, track projects, and deliver
+                            exceptional results.
+                        </li>
+                    </ol>
+                </main>
+
+                {/* Footer Section */}
+                <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center absolute bottom-5 w-full">
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="/contact"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <EnvelopeIcon className="w-5 h-5" aria-hidden="true" />
+                        Contact
+                    </a>
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="/about"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <UserCircleIcon
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                        />
+                        About
+                    </a>
+                    <a
+                        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                        href="/privacy-policy"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <ShieldCheckIcon
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                        />
+                        Privacy Policy
+                    </a>
+                </footer>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
