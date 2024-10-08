5a7330e407ae565c6b0b667b0798addc005ecbc2
DigitalHerencia <digitalherencia@outlook.com>
2024-10-08 08:59:51 -0600

    getting there

--- src/app/SessionWrapper.tsx
+++ src/app/SessionWrapper.tsx
@@ -2,7 +2,20 @@
 
 import { SessionProvider } from "next-auth/react"
 import { ReactNode } from "react"
+import { JSX } from "react/jsx-runtime"
 
-export default function SessionWrapper({ children }: { children: ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>
+/**
+ * A wrapper component that provides the NextAuth.js session context to its child components.
+ *
+ * @param {Object} props - The props object.
+ * @param {ReactNode} props.children - The child components to wrap.
+ * @returns {JSX.Element} The wrapped child components.
+ */
+export default function SessionWrapper({
+    children,
+}: {
+    children: ReactNode
+}): JSX.Element {
+    // eslint-disable-next-line react/no-children-prop
+    return <SessionProvider children={undefined}>{children}</SessionProvider>
 }
