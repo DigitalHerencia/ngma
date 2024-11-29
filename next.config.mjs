dee31b4441f1f9dfd80cbcac995847d3623659b1
Ivan Roman <DigitalHerencia@Outlook.com>
2024-11-29 15:07:49 -0700

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, // Enables React Strict Mode
-    images: {
-        domains: ["your-image-domain.com"], // Add external image domains here
-    },
-};
-
-export default nextConfig;
